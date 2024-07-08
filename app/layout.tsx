4117c73696941ef735b60229052008e96415d634
braile <braile.c.gawigawen@gmail.com>
2024-07-08 13:38:18 +0800

    [UI] update html class dark to make dark mode default

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="h-full antialiased " suppressHydrationWarning>
+    <html
+      lang="en"
+      className="h-full antialiased dark"
+      suppressHydrationWarning>
       <body
         className={`${inter.className} flex h-full bg-zinc-50 dark:bg-black`}>
         <div className="fixed inset-0 flex justify-center sm:px-8">
