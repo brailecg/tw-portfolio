d94aaa81ac94f4164b1200c568d6aa604ddef5af
braile <braile.c.gawigawen@gmail.com>
2025-11-01 10:36:10 +0800

    [FIX] update personal data type with contentTypeId error

--- types/contentful/TypePersonalDetails.ts
+++ types/contentful/TypePersonalDetails.ts
@@ -1,16 +1,25 @@
 import type { Entry, EntryFields } from "contentful";
 
 export interface TypePersonalDetailsFields {
-    homeTitle?: EntryFields.Symbol;
-    homeIntro?: EntryFields.Text;
-    aboutTitle?: EntryFields.Symbol;
-    aboutIntro?: EntryFields.Text;
-    experienceTitle?: EntryFields.Symbol;
-    experienceIntro?: EntryFields.Text;
-    projectsTitle?: EntryFields.Symbol;
-    projectsIntro?: EntryFields.Text;
-    blogsTitle?: EntryFields.Symbol;
-    blogsIntro?: EntryFields.Text;
+  homeTitle?: EntryFields.Symbol;
+  homeIntro?: EntryFields.Text;
+  aboutTitle?: EntryFields.Symbol;
+  aboutIntro?: EntryFields.Text;
+  experienceTitle?: EntryFields.Symbol;
+  experienceIntro?: EntryFields.Text;
+  projectsTitle?: EntryFields.Symbol;
+  projectsIntro?: EntryFields.Text;
+  blogsTitle?: EntryFields.Symbol;
+  blogsIntro?: EntryFields.Text;
+}
+export interface TypeProjectSys {
+  id: string;
 }
 
-export type TypePersonalDetails = Entry<TypePersonalDetailsFields>;
+export type PersonalDetailsEntry = {
+  sys: TypeProjectSys;
+  fields: TypePersonalDetailsFields;
+  contentTypeId: string;
+};
+
+export type TypePersonalDetails = Entry<PersonalDetailsEntry>;
